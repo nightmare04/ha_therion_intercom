6b2051c780a3ba06b71861a5d4fcaf363abe0990
nightmare <04nightmare86@gmail.com>
2024-01-31 09:52:18 +0900

    add const

--- custom_components/therion_intercom/const.py
+++ custom_components/therion_intercom/const.py
@@ -1,1 +1,15 @@
+"""Constant for therion intercom."""
+
 DOMAIN = "therion_intercom"
+URL = "https://mb.therion.org"
+URL_CODE = "/api/v1/auth/code"
+URL_LOGIN = "/api/v1/auth/login"
+URL_DATA = "/api/v1/contract"
+
+HEADERS = {
+    "Host": "mb.therion.org",
+    "Content-Type": "application/json",
+    "Connection": "keep-alive",
+    "Accept": "*/*",
+    "User-Agent": "Terion/1.0.9 (ru.televox.intercom; build:3; iOS 17.2.0) Alamofire/5.4.3",
+}
