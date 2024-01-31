5e1b4514e410cad212ffd84899b3361395ab6888
nightmare <04nightmare86@gmail.com>
2024-01-31 22:49:17 +0900

    add balance sensor

--- custom_components/therion_intercom/__init__.py
+++ custom_components/therion_intercom/__init__.py
@@ -1,33 +1,37 @@
-"""Init for therion intercom."""
+"""API from therion intercom."""
 
-import logging
+import aiohttp
 
-from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
-
-from .const import DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
+from .const import HEADERS, URL, URL_CODE, URL_LOGIN
 
 
-async def async_setup_entry(
-    hass: HomeAssistant, config_entry: config_entries.ConfigEntry
-) -> bool:
-    """Set up platform from a ConfigEntry."""
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
-
-    # Forward the setup to the camera platform.
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(config_entry, "camera")
-    )
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(config_entry, "button")
-    )
-    return True
+async def send_telephone(data) -> str:
+    """Send telephone number to get code."""
+    payload = {}
+    headers = {}
+    api_utl = URL + URL_CODE
+    payload["phone"] = data["phone"]
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            api_utl, json=payload, headers=HEADERS, ssl=False
+        ) as resp:
+            headers["Cookie"] = resp.headers.get("Set-Cookie")
+            print(resp.status)
+            return headers["Cookie"]
 
 
-async def async_remove_entry(hass: HomeAssistant, entry) -> None:
-    """Handle removal of an entry."""
-    await hass.config_entries.async_forward_entry_unload(entry, "camera")
-    await hass.config_entries.async_forward_entry_unload(entry, "button")
+async def get_token(data):
+    """Send telephone and sms code to get token."""
+    payload = {}
+    api_url = URL + URL_LOGIN
+    HEADERS["Cookie"] = data["Cookie"]
+    payload["phone"] = data["phone"]
+    payload["code"] = data["code"]
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            api_url, json=payload, headers=HEADERS, ssl=False
+        ) as resp:
+            print(resp.status)
+            json_resp = await resp.json()
+            token = "Bearer " + json_resp["accessToken"]
+            return token
---
+++ custom_components/therion_intercom/sensor.py
@@ -0,0 +1,78 @@
+"""Therion intercom camera."""
+
+from __future__ import annotations
+
+from datetime import timedelta
+import logging
+
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN, HEADERS, URL, URL_DATA
+
+_LOGGER = logging.getLogger(__name__)
+UPDATE_INTERVAL = timedelta(seconds=15)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> bool:
+    """Setup sensor balance from a config entry created in the integrations UI."""  # noqa: D401
+    entry = hass.data[DOMAIN][entry.entry_id]
+    session = async_get_clientsession(hass)
+    async_add_entities([SensorBalance(session, entry)], update_before_add=True)
+    return True
+
+
+class SensorBalance(SensorEntity):
+    """Therion intercom balance sensor."""
+
+    _attr_name = "Balance"
+    _attr_native_unit_of_measurement = "RUB"
+    _attr_icon = "mdi:currency-rub"
+    _attr_device_class = SensorDeviceClass.MONETARY
+    _attr_state_class = SensorStateClass.MEASUREMENT
+
+    def __init__(self, session, config) -> None:
+        """Init intercom balance."""
+        super().__init__()
+        self.config = config
+        self.session = session
+        self.last_contract = {}
+
+    @property
+    def name(self) -> str:
+        """Return name of Therion balance sensor."""
+        return "Balance sensor"
+
+    async def async_get_contract(self):
+        """Get contract info from API."""
+        payload = {}
+        headers = HEADERS
+        headers["Cookie"] = self.config["Cookie"]
+        headers["Authorization"] = self.config["token"]
+        api_url = URL + URL_DATA
+        async with self.session.get(api_url, json=payload, headers=headers) as resp:
+            json_data = await resp.json()
+            self.last_contract = json_data
+            return json_data
+
+    async def async_update(self):
+        """Update balance."""
+        data = await self.async_get_contract()
+        self._attr_native_value = data["content"][0]["balance"] or "0"
+
+    @property
+    async def async_unique_id(self) -> str:
+        """Return the unique ID of the sensor."""
+        await self.async_get_contract()
+        return self.last_contract["content"][0]["entrances"][0]["id"] + "balance"
