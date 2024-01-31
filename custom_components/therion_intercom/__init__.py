70291c4e9687f0cf269506f9638c72cb29a5833c
nightmare <04nightmare86@gmail.com>
2024-01-31 22:56:41 +0900

    add balance sensor

--- custom_components/therion_intercom/__init__.py
+++ custom_components/therion_intercom/__init__.py
@@ -1,37 +1,36 @@
-"""API from therion intercom."""
+"""Init for therion intercom."""
 
-import aiohttp
+import logging
 
-from .const import HEADERS, URL, URL_CODE, URL_LOGIN
+from homeassistant import config_entries, core
 
+from .const import DOMAIN
 
-async def send_telephone(data) -> str:
-    """Send telephone number to get code."""
-    payload = {}
-    headers = {}
-    api_utl = URL + URL_CODE
-    payload["phone"] = data["phone"]
-    async with aiohttp.ClientSession() as session:
-        async with session.post(
-            api_utl, json=payload, headers=HEADERS, ssl=False
-        ) as resp:
-            headers["Cookie"] = resp.headers.get("Set-Cookie")
-            print(resp.status)
-            return headers["Cookie"]
+_LOGGER = logging.getLogger(__name__)
 
 
-async def get_token(data):
-    """Send telephone and sms code to get token."""
-    payload = {}
-    api_url = URL + URL_LOGIN
-    HEADERS["Cookie"] = data["Cookie"]
-    payload["phone"] = data["phone"]
-    payload["code"] = data["code"]
-    async with aiohttp.ClientSession() as session:
-        async with session.post(
-            api_url, json=payload, headers=HEADERS, ssl=False
-        ) as resp:
-            print(resp.status)
-            json_resp = await resp.json()
-            token = "Bearer " + json_resp["accessToken"]
-            return token
+async def async_setup_entry(
+    hass: core.HomeAssistant, config_entry: config_entries.ConfigEntry
+) -> bool:
+    """Set up platform from a ConfigEntry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
+
+    # Forward the setup to the camera, button, sensor platform.
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config_entry, "camera")
+    )
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config_entry, "button")
+    )
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    )
+    return True
+
+
+async def async_remove_entry(hass, entry) -> None:
+    """Handle removal of an entry."""
+    await hass.config_entries.async_forward_entry_unload(entry, "camera")
+    await hass.config_entries.async_forward_entry_unload(entry, "button")
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
